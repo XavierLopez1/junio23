bd50f78da799fb6960c1abb111cb7b901a2c660f
XavierLopez1 <lopez.velasco.xavier@gmail.com>
2023-06-08 22:15:16 -0600

    tercer commit

--- cadenas.java
+++ cadenas.java
@@ -5,15 +5,15 @@ public class cadenas {
         Scanner teclado=new Scanner(System.in);
         String nombre1,nombre2;
         int edad1,edad2;
-        System.out.print("Ingrese el nombre:"); //ingresa el nombre del primer usuario
+        System.out.print("Ingrese el nombre: "); //ingresa el nombre del primer usuario
         nombre1=teclado.next();
-        System.out.print("Ingrese edad:");
+        System.out.print("Ingrese edad:"); //ingresa la edad del primer usuario
         edad1=teclado.nextInt();
-        System.out.print("Ingrese el nombre:");
+        System.out.print("Ingrese el nombre: ");//ingresa el nombre del segundo usuario
         nombre2=teclado.next();
-        System.out.print("Ingrese edad:");
+        System.out.print("Ingrese edad: ");//ingresa la edad del segundo usuario
         edad2=teclado.nextInt();
-        System.out.print("La persona de mayor edad es:");
+        System.out.print("La persona de mayor edad es: ");
         if (edad1>edad2) {
             System.out.print(nombre1);
         } else {
